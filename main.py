888a792bbbcccbe4819c6f4431bfc209ab9a5df2
nhinguyen <nguyenhis@gmail.com>
2023-08-30 13:06:08 +0300

    2_4 second  commit

--- main.py
+++ main.py
@@ -4,6 +4,4 @@ number_3 = int(input("Enter number 3: "))
 sum = number_1 + number_2 + number_3
 product = number_1 * number_2 * number_3
 average = float(sum/3)
-print("The sum of 3 numbers is:",sum)
-print("The product of 3 numbers is:",product)
-print(f"The average of 3 numbers is: {average:.2f}")
+print(f"The average of 3 numbers is: {average:.2f}\nThe sum of 3 numbers is: {sum}\nThe product of 3 numbers is: {product}")
