836abc7f5f8962ed5f33f6eafda7c68a8931f9cd
nhinguyen <nguyenhis@gmail.com>
2023-08-30 12:42:18 +0300

    2_4 commit

---
+++ main.py
@@ -0,0 +1,9 @@
+number_1 = int(input("Enter number 1: "))
+number_2 = int(input("Enter number 2: "))
+number_3 = int(input("Enter number 3: "))
+sum = number_1 + number_2 + number_3
+product = number_1 * number_2 * number_3
+average = float(sum/3)
+print("The sum of 3 numbers is:",sum)
+print("The product of 3 numbers is:",product)
+print(f"The average of 3 numbers is: {average:.2f}")
